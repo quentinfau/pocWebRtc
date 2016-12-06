0948e42ad8cf6b990e848fe689fcb81aaf521522
Quent <quentin.fau74@gmail.com>
2016-12-06 09:00:23 +0100

    azerty

--- ws-server.js
+++ ws-server.js
@@ -3,7 +3,7 @@ const SocketServer = require('ws').Server;
 const path = require('path');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+const INDEX = path.join(__dirname, 'test.html');
 
 const server = express()
 		.use((req, res) => res.sendFile(INDEX) )
